2c46d2a5d62e30692f23581a6a165a15547f1879
jun-kondo <ranmaru.oo.q@gmail.com>
2024-02-08 19:53:53 +0900

    [modify]main.rb RuboCopチェック

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-json_data = File.open("memos.json", "r") do |f|
-  JSON.load(f)
-end
-memos = json_data["memos"]
+json_data = JSON.load_file('memos.json')
+memos = json_data['memos']
 
 helpers do
   def link_to(text, url)
@@ -24,11 +24,11 @@ end
 
 post '/memos' do
   last_memo = memos.last
-  latest_id = last_memo["id"].to_i.succ
+  latest_id = last_memo['id'].to_i.succ
   new_memo = Hash.new([])
-  new_memo["id"] = latest_id.to_s
-  new_memo["title"] = params[:title]
-  new_memo["body"] = params[:body]
+  new_memo['id'] = latest_id.to_s
+  new_memo['title'] = params[:title]
+  new_memo['body'] = params[:body]
   memos << new_memo
   File.write 'memos.json', json_data.to_json
   redirect '/memos'
@@ -36,6 +36,6 @@ post '/memos' do
 end
 
 get '/memos/:id' do
-  @memo = memos.find {|memo| memo['id'] == params[:id]}
+  @memo = memos.find { |memo| memo['id'] == params[:id] }
   erb :show
 end
