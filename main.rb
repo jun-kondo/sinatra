b519195e593986ec288a1182ea097ab7accff7bd
jun-kondo <ranmaru.oo.q@gmail.com>
2024-02-08 16:13:27 +0900

    [Add]メモ一覧を表示
    jsonファイルのテストデータをビューに表示

--- main.rb
+++ main.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
+
+json_data = File.open("memos.json", "r") do |f|
+  JSON.load(f)
+end
+memos = json_data["memos"]
+
+helpers do
+  def link_to(text, url)
+    "<a href=#{url}>#{text}"
+  end
+end
 
 get '/memos' do
+  @memos = memos
   erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos' do
+  redirect '/memos'
+  erb :new
 end
