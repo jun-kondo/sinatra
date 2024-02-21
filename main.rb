1a2f3eb1b074f4e3b81e055a46bbabdac27f220a
jun-kondo <ranmaru.oo.q@gmail.com>
2024-02-22 04:15:39 +0900

    [Modify]リファクタリング

--- main.rb
+++ main.rb
@@ -8,11 +8,15 @@ require 'cgi'
 require 'securerandom'
 require 'pg'
 
-PG::Connection.new(dbname: 'memo').exec("CREATE TABLE IF NOT EXISTS memo(
-                                      id CHAR(36) NOT NULL,
-                                      title VARCHAR(20) NOT NULL,
-                                      body VARCHAR(100),
-                                      PRIMARY KEY (id));")
+DB_NAME = 'memo_app'
+TABLE_NAME = 'memo'
+conn = PG::Connection.new(dbname: DB_NAME)
+conn.exec("CREATE TABLE IF NOT EXISTS memo(
+            id CHAR(36) NOT NULL,
+            title VARCHAR(100) NOT NULL,
+            body VARCHAR(1000),
+            PRIMARY KEY (id));")
+conn.close
 
 helpers do
   def link_to(text, url)
@@ -25,8 +29,9 @@ get '/' do
 end
 
 get '/memos' do
-  conn = PG::Connection.new(dbname: 'memo')
-  @memos = conn.exec('SELECT * FROM memo;').to_a
+  conn = PG::Connection.new(dbname: DB_NAME)
+  @memos = conn.exec("SELECT * FROM #{TABLE_NAME};").to_a
+  conn.close
   erb :index
 end
 
@@ -36,31 +41,42 @@ end
 
 post '/memos' do
   id = SecureRandom.uuid
-  conn = PG::Connection.new(dbname: 'memo')
-  conn.exec_params('INSERT INTO memo VALUES($1, $2, $3);', [id, params['title'], params['body']])
+  conn = PG::Connection.new(dbname: DB_NAME)
+  conn.exec_params("INSERT INTO #{TABLE_NAME} VALUES($1, $2, $3);", [id, params['title'], params['body']])
+  conn.close
   redirect '/memos'
 end
 
 get '/memos/:id' do
-  conn = PG::Connection.new(dbname: 'memo')
-  @memo = conn.exec_params('SELECT * FROM memo WHERE id=$1;', [params['id']]).to_a[0]
+  conn = PG::Connection.new(dbname: DB_NAME)
+  @memo = conn.exec_params("SELECT * FROM #{TABLE_NAME} WHERE id = $1;", [params['id']]).to_a[0]
+  conn.close
   erb :show
 end
 
 get '/memos/:id/edit' do
-  conn = PG::Connection.new(dbname: 'memo')
-  @memo = conn.exec_params('SELECT * FROM memo WHERE id=$1;', [params['id']]).to_a[0]
+  conn = PG::Connection.new(dbname: DB_NAME)
+  @memo = conn.exec_params("SELECT * FROM #{TABLE_NAME} WHERE id = $1;", [params['id']]).to_a[0]
+  conn.close
   erb :edit
 end
 
 patch '/memos/:id' do
-  conn = PG::Connection.new(dbname: 'memo')
-  conn.exec_params('UPDATE memo SET title=$1, body=$2 WHERE id=$3;', [params['title'], params['body'], params['id']])
+  conn = PG::Connection.new(dbname: DB_NAME)
+  conn.exec_params("UPDATE #{TABLE_NAME} SET title = $1, body = $2 WHERE id = $3;", [params['title'], params['body'], params['id']])
+  conn.close
   redirect '/memos'
 end
 
 delete '/memos/:id' do
-  conn = PG::Connection.new(dbname: 'memo')
-  conn.exec_params('DELETE FROM memo WHERE id=$1;', [params['id']])
+  conn = PG::Connection.new(dbname: DB_NAME)
+  conn.exec_params("DELETE FROM #{TABLE_NAME} WHERE id = $1;", [params['id']])
+  conn.close
   redirect '/memos'
+end
+
+private
+
+def connect_db
+  PG::Connection.new(dbname: DB_NAME)
 end
